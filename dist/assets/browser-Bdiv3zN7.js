54e63d5e9bad789f74e606dcb07f5f2a7ae39f1b
Daniel Abrams <immortality.bytes.book@gmail.com>
2025-08-22 17:06:54 -0400

    SettingUpBoltCursor

---
+++ dist/assets/browser-Bdiv3zN7.js
@@ -0,0 +1,2 @@
+import{f as e}from"./vendor-DknaEY1O.js";const o=e((function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")})),t=Object.freeze(Object.defineProperty({__proto__:null,default:o},Symbol.toStringTag,{value:"Module"}));export{t as b};
+//# sourceMappingURL=browser-Bdiv3zN7.js.map
